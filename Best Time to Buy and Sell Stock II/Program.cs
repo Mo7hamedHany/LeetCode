51ac96dc7a20db5bc49591b55fb6d2e8dfdf891a
Mei <Mei@DESKTOP-RQGE80T>
2025-12-16 10:34:25 +0200

    Merge Sorted Array II

---
+++ Best Time to Buy and Sell Stock II/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Best_Time_to_Buy_and_Sell_Stock_II
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           var solution = new Solution();
+
+             int[] prices = { 7, 1, 5, 3, 6, 4 };
+
+            int maxProfit = solution.MaxProfit(prices);
+
+            Console.WriteLine($"Max Profit: {maxProfit}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Best Time to Buy and Sell Stock II/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Best_Time_to_Buy_and_Sell_Stock_II
+{
+    public class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int profit = 0;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] > prices[i - 1])
+                {
+                    profit += prices[i] - prices[i - 1];
+                }
+            }
+
+            return profit;
+        }
+    }
+}
