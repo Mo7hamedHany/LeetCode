5db1285d57a42bd699ce1a1422144157afdd3eff
Mei <Mei@DESKTOP-RQGE80T>
2025-12-17 14:19:40 +0200

    Insert Delete GetRando O(1)

---
+++ Insert Delete GetRandom O(1)/RandomizedSet.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insert_Delete_GetRandom_O_1_
+{
+    public class RandomizedSet
+    {
+
+        public RandomizedSet()
+        {
+
+        }
+
+        List<object> nums = new List<object>();
+
+        public bool Insert(int val)
+        {
+            if (nums.Contains(val))
+                return false;
+
+            nums.Add(val);
+
+            return true;
+        }
+
+        public bool Remove(int val)
+        {
+            if (!nums.Contains(val))
+                return false;
+
+            nums.Remove(val);
+
+            return true;
+        }
+
+        public int GetRandom()
+        {
+            Random rand = new Random();
+            int index = rand.Next(0, nums.Count);
+            return (int)nums[index];
+        }
+    }
+}
