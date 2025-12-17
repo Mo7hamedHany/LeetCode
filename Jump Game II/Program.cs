0b016ebaeed3c224e8bcda118f1e491047a8d80d
Mei <Mei@DESKTOP-RQGE80T>
2025-12-17 11:10:32 +0200

    Jump Game II

---
+++ Jump Game II/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Jump_Game_II
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] nums = { 2, 3, 1, 1, 4 };
+            int result = solution.Jump(nums);
+            Console.WriteLine(result); 
+        }
+    }
+}
